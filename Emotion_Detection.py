ab51f663f2fdeb65e4fc2d4de78d0e28a05f07a4
Mondher_OneForAll <monder850@gmail.com>
2023-06-27 01:17:00 +0200

    Added Emotion Detection

---
+++ Emotion_Detection.py
@@ -0,0 +1,77 @@
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.image import Image
+from kivymd.uix.screen import Screen
+from kivymd.uix.button import MDRaisedButton
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
+import numpy as np
+from keras.utils import img_to_array
+from keras.models import model_from_json
+
+model = model_from_json(open("Resources/facial_expression_model_structure.json", 'r').read())
+model.load_weights("Resources/facial_expression_model_weights.h5")
+
+Window.size = (360, 600)
+
+
+class CameraApp(MDApp):
+
+    def build(self):
+        self.count = 0
+        screen = Screen()
+        layout = MDBoxLayout(orientation="vertical")
+        self.image = Image()
+        self.face_Cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        button = MDRaisedButton(text="Click here",
+                                pos_hint={"center_x": 0.5, "center_y": 0.5},
+                                size_hint=(None, None),
+                                on_release=self.take_picture)
+
+        layout.add_widget(self.image)
+        layout.add_widget(button)
+        screen.add_widget(layout)
+
+        self.cap = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.load_video, 1.0 / 60.0)
+
+        return screen
+
+    def load_video(self, *args):
+        emotions = ("angry", "disgust", "fear", "happy", "sad", "suprise", "neutral")
+        _, img = self.cap.read()
+        self.frame = img
+        imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        face = self.face_Cascade.detectMultiScale(imgGray, 1.5, 4)
+        for (x, y, w, h) in face:
+            roi_gray = imgGray[y: y + h, x: x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48))
+            img_pixels = img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+
+            img_pixels /= 255
+            predictions = model.predict(img_pixels)
+            max_index = np.argmax(predictions[0])
+            emotion = emotions[max_index]
+            print(emotion)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+            cv2.putText(img, emotion, (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
+
+        buffer = cv2.flip(img, 0).tostring()
+        texture = Texture.create(size=(img.shape[1], img.shape[0]), colorfmt="bgr")
+        texture.blit_buffer(buffer, colorfmt="bgr", bufferfmt="ubyte")
+        self.image.texture = texture
+
+    def take_picture(self, *args):
+        image_id = "picture " + str(self.count) + ".jpg"
+        cv2.imwrite("Camera_Pictures/" + image_id, self.frame)
+        cv2.rectangle(self.frame, (0, 50), (300, 100), (0, 255, 0), cv2.FILLED)
+        cv2.putText(self.frame, "Scan Saved", (10, 50), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 255), 2)
+        cv2.waitKey(500)
+        self.count += 1
+
+
+if __name__ == '__main__':
+    CameraApp().run()
--- WaterMark_Remover.py
+++ WaterMark_Remover.py
@@ -16,7 +16,7 @@ class WaterMarkRemoverApp(MDApp):
         self.theme_cls.theme_style = "Dark"
         screen = Screen()
         layout = MDBoxLayout(orientation="vertical")
-        self.image = Image(source="Resources/mainWm.jpg")
+        self.image = Image(source="Resources/BG.png")
         button = MDRaisedButton(text="Click here",
                                 pos_hint={"center_x": 0.5, "center_y": 0.5},
                                 size_hint=(None, None),
@@ -29,7 +29,7 @@ class WaterMarkRemoverApp(MDApp):
 
     def remove_WaterMark(self, *args):
         src = cv2.imread(self.image.source)
-        mask = cv2.imread("Resources/Wm.png", cv2.IMREAD_GRAYSCALE)  # put here the WaterMark to remove
+        mask = cv2.imread("Resources/WM.png", cv2.IMREAD_GRAYSCALE)  # put here the WaterMark to remove
         (h, w, _) = src.shape
         mask = cv2.resize(mask, (w, h))
         dest = cv2.inpaint(src, mask, 3, cv2.INPAINT_NS)
